c33ac27bac30d99b9f98348c51db76246610d138
Bastien <b.cabirou@gmail.com>
2017-07-29 12:54:48 +1000

    add bootstrap to the project files

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 const apiUrl = 'https://us-central1-datapocalypse-c3889.cloudfunctions.net/';
+const apiCSV = '';
 
 // Replace the text from the Dropdown when select an item
 $(".suburb-dropdown .dropdown-menu button").click(function() {
     $(".suburb-dropdown .btn:first-child").text($(this).text());
     $(".suburb-dropdown .btn:first-child").val($(this).text());
 });
-
 
 function getSurvivalData() {
     $.get(apiUrl + '/getSurvivalData', function(data) {
@@ -18,7 +18,21 @@ function displayData() {
 
 }
 
+(function getData() {
 
-(function getSuburbs() {
+    $.get('/population_extract.csv', function(data) {
+        // $(".result").html(data);
+        // var data2 = $.csv.toArray(data);
+        console.log("Population data: " + data2);
+    });
+
+
+    function initSuburbsDropdown() {
+
+    }
+
+    function createSuburbButton(suburbName) {
+        return '<button class="dropdown-item">' + suburbName + '</button>'
+    };
 
 })();
