3e6e644b46ed7184a486acddcc4ca8decc01b611
Bastien <b.cabirou@gmail.com>
2017-07-29 12:04:46 +1000

    add base api to get survival data

--- public/app.js
+++ public/app.js
@@ -1,5 +1,19 @@
+const apiUrl = 'https://us-central1-datapocalypse-c3889.cloudfunctions.net/';
+
 // Replace the text from the Dropdown when select an item
 $(".suburb-dropdown .dropdown-menu button").click(function() {
     $(".suburb-dropdown .btn:first-child").text($(this).text());
     $(".suburb-dropdown .btn:first-child").val($(this).text());
 });
+
+
+function getSurvivalData() {
+    $.get(apiUrl + '/getSurvivalData', function(data) {
+        // $(".result").html(data);
+        console.log("Survival Rate: " + data.index);
+    });
+}
+
+function displayData() {
+
+}
