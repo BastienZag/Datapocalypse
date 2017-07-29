b59d2d767b4e74bf67831e3afce537f23659b0df
U2AIT <linda20138@yahoo.com.au>
2017-07-29 13:33:09 +1000

    update html

---
+++ public/map.js
@@ -0,0 +1,11 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+var mapEle=document.getElementsByName("mapid");
+
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZGF0YXBvY2FseXBzZSIsImEiOiJjajVvbmp5eDIwMXZ6MzNxbWM1OHkxZDF2In0.pGgODeyy-u8IwlqXUHCYPg'
+}).addTo(mymap);
